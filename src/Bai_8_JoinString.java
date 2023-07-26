39940538a2595b62bc4190f91c4b2ad55babff11
sSoRaK <thong9515@gmail.com>
2023-07-26 19:58:23 +0900

    upload ex-08

--- src/Bai_8_JoinString.java
+++ src/Bai_8_JoinString.java
@@ -3,7 +3,24 @@ import java.util.Scanner;
 public class Bai_8_JoinString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập một chuỗi: ");
-        String str = scanner.nextLine();
+        System.out.print("Nhập chuỗi thứ nhất: ");
+        String str1 = scanner.nextLine();
+        System.out.print("Nhập chuỗi thứ hai: ");
+        String str2 = scanner.nextLine();
+        String outputString = "";
+        // nối chuỗi khi độ dài 2 chuỗi bằng nhau
+        if (str1.length() == str2.length()) {
+            outputString = str1.concat(str2);
+        }
+        // tìm độ dài chuỗi lớn hơn và cắt bỏ sau đó nối lại
+        int maxLength = Math.max(str1.length(), str2.length());
+        if (maxLength == str1.length()) {
+            int cutLength = str1.length() - str2.length();
+            outputString = str1.substring(cutLength).concat(str2);
+        } else {
+            int cutLength = str2.length() - str1.length();
+            outputString = str2.substring(cutLength).concat(str1);
+        }
+        System.out.println("Chuỗi sau khi nối: " + outputString);
     }
 }
